a67e1bb981c5f4704c4aeaf6a688975c924ebe3c
Alhoceima Ahamjik Lorenzo <alhoceima.ahamjik.lorenzo@everis.nttdata.com>
2022-02-26 20:35:48 +0100

    Subo últimas actualizaciones

--- force-app/main/default/lwc/yN_SHOPPING_LIST/yN_SHOPPING_LIST.js
+++ force-app/main/default/lwc/yN_SHOPPING_LIST/yN_SHOPPING_LIST.js
@@ -8,6 +8,48 @@
 import { LightningElement } from 'lwc';
 
 export default class YN_SHOPPING_LIST extends LightningElement {
+    productValue;
+    amountValue;
 
+    /*Recoger las recomendaciones y actualizar la lista */
+
+    cleanMessage( cmpName){
+        const error=this.template.querySelector(cmpName);
+        error.setCustomValidity("");
+        error.reportValidity();
+    }
+    productChangeValue(event){
+        this.productValue=event.target.value;
+       this.cleanMessage(".productCSS");
+    }
+    amountChangeValue(event){
+        this.amountValue=event.target.value;
+        this.cleanMessage(".amountCSS");
+    }
+
+    handleClick(){
+        
+        if(this.productValue!=null && this.productValue!='' && this.amountValue>0){
+            let targetDiv=this.template.querySelector(".list");
+            let content=document.createTextNode("datos:"+this.productValue+"- "+this.amountValue);
+           
+            targetDiv.appendChild(content);
+            // enviar a Apex
+           
+        }else{
+            if(this.productValue==null || this.productValue=='' ){
+                const prodError=this.template.querySelector(".productCSS");
+                 prodError.setCustomValidity("Review this data");
+                 prodError.reportValidity();
+                
+            }
+            if(this.amountValue<=0 || this.amountValue==null ||this.amountValue==''){
+                const amountError=this.template.querySelector(".amountCSS");
+                 amountError.setCustomValidity("Review this data");
+                 amountError.reportValidity();
+                
+            }
+        }
+    }
     
 }
